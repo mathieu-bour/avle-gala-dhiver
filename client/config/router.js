d1ab8d420476fe22e740a3e2833eef07e7492bb9
ladislas14 <ladislas14@gmail.com>
2015-12-02 21:54:25 +0100

    ghbkz

--- client/config/router.js
+++ client/config/router.js
@@ -73,32 +73,11 @@ var delta = open.diff(now);
 if(delta <= 0) {
     Router.route("/buy", {
         name: "buy",
-        link: stylesheets.front,
-        data: function () {
-            var query = this.params.query;
-            var ticketsNb = Tickets.find().count();
-            if(query.id && ticketsNb <= 700){
-                return true
-            }else{
-                Session.set("error", "Nous sommes désolés mais il n'y a plus de places disponibles pour le Gala d'hiver.")
-                Router.go('/');
-            }
-
-        }
+        link: stylesheets.front
     });
     Router.route("/buy/payment", {
         name: "payment",
-        link: stylesheets.front,
-        data: function () {
-            var query = this.params.query;
-            var ticketsNb = Tickets.find().count();
-            if(query.id && ticketsNb <= 700){
-                return Tickets.findOne(query.id);
-            }else{
-                Router.go('/');
-            }
-
-        }
+        link: stylesheets.front
     });
     Router.route("/buy/payment/validate", {
         name: "validate",
